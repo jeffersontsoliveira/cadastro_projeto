f9ea97609e523431b204cb387155efd7798d72ed
jeffersontsoliveira <jeffersondeoliveira2017@gmail.com>
2021-10-13 23:50:26 -0400

    versão beta

---
+++ src/models/user.py
@@ -0,0 +1,18 @@
+from scr.DataBase.database import BaseModel
+from datetime import datetime
+import peewee
+
+class User(BaseModel):
+    name = peewee.CharField()
+    username = peewee.CharField()
+    password = peewee.CharField()
+    email = peewee.CharField(unique=True)
+    admim = peewee.BooleanField(default=False)
+
+    createdAt = peewee.DateTimeField(default = datetime.utcnow())
+    updatedAt = peewee.DateTimeField(default = datetime.utcnow())
+
+    class Meta:
+        table_name = '_user'
+
+
---
+++ src/routes/__init__.py
@@ -0,0 +1,8 @@
+from sanic import Blueprint
+from scr.routes.user import user
+
+
+routes = Blueprint.group([user], url_prefix='/app')
+
+
+
